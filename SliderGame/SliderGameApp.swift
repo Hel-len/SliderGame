8fd8015f4258c4da0f27e914af90748a5731a46b
Lena <eveforeve@yandex.ru>
2021-06-11 14:51:27 +0500

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Елена Дранкина on 11.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
